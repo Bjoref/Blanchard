5ca71f7bb726288ce8191083f70e60ab61d6f506
Bjoref <flamros@yandex.ru>
2024-03-06 18:54:52 +0500

    work in progress

--- src/js/index.js
+++ src/js/index.js
@@ -16,18 +16,3 @@ const swiper = new Swiper('.swiper', {
 		enabled: true,
 	},
 });
-
-const items = document.querySelectorAll('.item');
-
-const reset = () => items.forEach((item) => item.classList.remove('animation'));
-
-function accordion(e) {
-  if (!e.target.closest('.item')) return;
-  reset();
-  e.target.classList.add('animation');
-}
-
-const init = () => items[items.length-1].classList.add('animation');
-
-window.addEventListener('load',init,false);
-window.addEventListener('click',accordion,false);
