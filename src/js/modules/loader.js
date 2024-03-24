46ceb531f03374d78c78b120248c3191bfb4610d
Bjoref <flamros@yandex.ru>
2024-03-25 00:42:05 +0500

    desk first -> mobile first header

--- src/js/modules/loader.js
+++ src/js/modules/loader.js
@@ -2,9 +2,7 @@ function loader() {
 	const loader = document.querySelector('#loader');
 
 	window.addEventListener('load', () => {
-        setTimeout(()=> {
-            loader.classList.add('loader-wrapper--hidden');
-        }, 1000)
+        loader.classList.add('loader-wrapper--hidden');
 	});
 }
 
