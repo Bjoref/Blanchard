7a24aa71d4803a8ecee8ae6cb75f8c3f92a9d86c
Bjoref <flamros@yandex.ru>
2024-03-26 15:10:49 +0500

    final

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,38 @@
-import loader from './modules/loader.js'
+import loader from "./modules/loader.js";
 
 loader();
 
-const sectionGalleryCheckbox = document.querySelectorAll('.section-gallery__form-control');
+const sectionGalleryCheckbox = document.querySelectorAll(
+  ".section-gallery__form-control"
+);
 
 sectionGalleryCheckbox.forEach((checkbox) => {
-	checkbox.addEventListener('input', () => {
-		if(checkbox.classList.contains('section-gallery__form-control_checked')) {
-			checkbox.classList.remove('section-gallery__form-control_checked')
-		} else {
-			checkbox.classList.add('section-gallery__form-control_checked')
-		}
-	})
-})
+  checkbox.addEventListener("input", () => {
+    if (checkbox.classList.contains("section-gallery__form-control_checked")) {
+      checkbox.classList.remove("section-gallery__form-control_checked");
+    } else {
+      checkbox.classList.add("section-gallery__form-control_checked");
+    }
+  });
+});
+
+function onEntry(entry) {
+  entry.forEach((change) => {
+	console.log(change.isIntersecting)
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+
+console.log(elements)
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
